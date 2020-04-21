43d26d36c9800eb1c31d693d09dfad73e558cd81
MCHR001 <MCHR001>
2020-04-21 07:39:26 +0000

    WHO_data demographics

--- R/01_load.R
+++ R/01_load.R
@@ -5,6 +5,7 @@ rm(list = ls())
 # Load libraries
 # ------------------------------------------------------------------------------
 library("tidyverse")
+library("stringr")
 
 # Define functions
 # ------------------------------------------------------------------------------
@@ -12,13 +13,29 @@ source(file = "R/99_project_functions.R")
 
 # Load data
 # ------------------------------------------------------------------------------
-my_data_raw <- read_tsv(file = "data/_raw/my_raw_data.tsv")
+#my_data_raw <- read_tsv(file = "data/_raw/my_raw_data.tsv")
+
+COVID_test_raw <- read_csv2(file = "data/_raw/Our world in data/covid-testing-all-observations.csv")
+POP_demo_raw <- read_csv(file = "data/_raw/WHO/Population demographics/Population demographics_all years.csv")
 
 # Wrangle data
 # ------------------------------------------------------------------------------
-my_data <- my_data_raw # %>% ...
+# my_data <- my_data_raw # %>% ...
+
+COVID_test <- COVID_test_raw # %>% ...
+
+POP_demo <- POP_demo_raw 
+POP_demo$`Population (in thousands) total` <- gsub('\\s+', '', POP_demo$`Population (in thousands) total`) 
+POP_demo$`Population (in thousands) total` <- as.numeric(POP_demo$`Population (in thousands) total`) 
+POP_demo$Year <- as.numeric(POP_demo$Year) 
+
 
 # Write data
 # ------------------------------------------------------------------------------
-write_tsv(x = my_data,
-          path = "data/01_my_data.tsv")
+# write_tsv(x = my_data, path = "data/01_my_data.tsv")
+
+write_csv(x = COVID_test,
+          path = "data/01_COVID_test.csv")
+
+write_csv(x = POP_demo,
+          path = "data/01_POP_demo.csv")
--- R/02_clean.R
+++ R/02_clean.R
@@ -12,13 +12,26 @@ source(file = "R/99_project_functions.R")
 
 # Load data
 # ------------------------------------------------------------------------------
-my_data <- read_tsv(file = "data/01_my_data.tsv")
+# my_data <- read_tsv(file = "data/01_my_data.tsv")
+
+COVID_test <- read_csv(file = "data/01_COVID_test.csv")
+POP_demo <- read_csv(file = "data/01_POP_demo.csv")
 
 # Wrangle data
 # ------------------------------------------------------------------------------
-my_data_clean <- my_data # %>% ...
+#my_data_clean <- my_data # %>% ...
 
+COVID_test_clean <- COVID_test %>%
+  rename("Country" = "Entity") %>% 
+  select(Country, Date, `Cumulative total`, `Cumulative total per thousand`)
+
+POP_demo_clean <- POP_demo %>% 
+  select(-`Population living on &lt;$1 (PPP int. $) a day (%)`) %>%
+ filter(Year %in% c("2020", "2013", "2016"))
+
+  
+
+  
 # Write data
 # ------------------------------------------------------------------------------
-write_tsv(x = my_data_clean,
-          path = "data/02_my_data_clean.tsv")
+#write_tsv(x = my_data_clean, path = "data/02_my_data_clean.tsv")
