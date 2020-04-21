bb166f63135191347a2bac5fac9dd895e1d7880f
metchr <mette.christoffersen.02@regionh.dk>
2020-04-21 08:03:57 +0000

    Updated files with WHO mortality data

--- R/01_load.R
+++ R/01_load.R
@@ -5,6 +5,9 @@ rm(list = ls())
 # Load libraries
 # ------------------------------------------------------------------------------
 library("tidyverse")
+library("stringr")
+library("lubridate")
+library("readxl")
 
 # Define functions
 # ------------------------------------------------------------------------------
@@ -12,13 +15,39 @@ source(file = "R/99_project_functions.R")
 
 # Load data
 # ------------------------------------------------------------------------------
-my_data_raw <- read_tsv(file = "data/_raw/my_raw_data.tsv")
+##WHO - mortality
+#Adult mortality
+adult_mortality_raw <- read_csv(file = "data/_raw/WHO/Mortality/Adult mortality.csv",  
+                                col_names = c("Country", "Year", "Adult mortality rate", "Adult male mortality rate", "Adult female mortality rate"),
+                                skip = 2)
+
+#Life expectancy and healthy life expectancy
+life_expectancy_raw <- read_csv(file = "data/_raw/WHO/Mortality/Life expectancy and healthy life expectancy.csv",
+                                col_names = c("Country", "Year", "Life expectancy at birth (years)", "Male life expectancy at birth (years)", "Female life expectancy at birth (years)", "Life expectancy at age 60 (years)", "Male life expectancy at age 60 (years)", "Female life expectancy at age 60 (years)", "Healthy life expectancy (HALE) at birth (years)", "Male healthy life expectancy (HALE) at birth (years)", "Female healthy life expectancy (HALE) at birth (years)", "Healthy life expectancy (HALE) at age 60 (years)", "Male healthy life expectancy (HALE) at age 60 (years)", "Female healthy life expectancy (HALE) at age 60 (years)"),
+                                skip = 2)
+
+#Cause specific mortality
+col_names <- read_xls(path = "data/_raw/WHO/Mortality/Cause_specific_deaths.xls",
+                      sheet = 2,
+                      cell_rows(7)) %>% 
+  names()
+
+mortality_causes_raw <- read_xls(path = "data/_raw/WHO/Mortality/Cause_specific_deaths.xls",
+                      sheet = 2,
+                      skip = 9,
+                      col_names = col_names)
+
 
 # Wrangle data
 # ------------------------------------------------------------------------------
-my_data <- my_data_raw # %>% ...
+
+
+
 
 # Write data
 # ------------------------------------------------------------------------------
-write_tsv(x = my_data,
-          path = "data/01_my_data.tsv")
+write_tsv(x = adult_mortality_raw,
+          path = "data/01_adult_mortality_load.tsv")
+
+write_tsv(x = life_expectancy_raw,
+          path = "data/01_life_expectancy_load.tsv")
--- R/02_clean.R
+++ R/02_clean.R
@@ -5,6 +5,9 @@ rm(list = ls())
 # Load libraries
 # ------------------------------------------------------------------------------
 library("tidyverse")
+library("stringr")
+library("lubridate")
+library("readxl")
 
 # Define functions
 # ------------------------------------------------------------------------------
@@ -12,13 +15,27 @@ source(file = "R/99_project_functions.R")
 
 # Load data
 # ------------------------------------------------------------------------------
-my_data <- read_tsv(file = "data/01_my_data.tsv")
+adult_mortality <- read_tsv(file = "data/01_adult_mortality_load.tsv")
+life_expedtancy <- read_tsv(file = "data/01_life_expectancy_load.tsv")
 
 # Wrangle data
 # ------------------------------------------------------------------------------
-my_data_clean <- my_data # %>% ...
+##WHO - mortality
+#Adult mortality
+adult_mortality_clean <- adult_mortality  %>% 
+  filter(Year == 2016) %>% 
+  select(Country, `Adult mortality rate`) 
+
+#Adult_mortality rate corresponds to the probability of dying between age 15 and 60 per 1000 individuals
+
+#Life expectancy and healthy life expectancy  
+life_expectancy_clean <- life_expectancy  %>% 
+  filter(Year == 2016) %>% 
+  select(Country, `Life expectancy at birth (years)`, `Healthy life expectancy (HALE) at birth (years)`) 
 
 # Write data
 # ------------------------------------------------------------------------------
-write_tsv(x = my_data_clean,
-          path = "data/02_my_data_clean.tsv")
+write_tsv(x = adult_mortality_clean,
+          path = "data/02_adult_mortality_clean.tsv")
+write_tsv(x = life_expectancy_clean,
+          path = "data/02_life_expectancy_clean.tsv")
