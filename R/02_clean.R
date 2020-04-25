516bc8e4c0651b85f9a8f249697f22c0e628e275
MCHR001 <MCHR001>
2020-04-25 22:52:49 +0000

    Cleaned - Johns Hopkins data

--- R/02_clean.R
+++ R/02_clean.R
@@ -118,9 +118,7 @@ JH_conftime_clean <- JH_conftime_clean %>%
       str_detect(Lat_Long, "41.1129/85.2401") ~ "40.1824/116.4142",
       str_detect(Lat_Long, "24.974/101.487") ~ "40.1824/116.4142",
       str_detect(Lat_Long, "29.1832/120.0934") ~ "40.1824/116.4142",
-      TRUE ~ Lat_Long
-      )
-) %>% 
+      TRUE ~ Lat_Long)) %>% 
   select(-`Province/State`) %>% 
   separate(Lat_Long, into = c("Lat", "Long"), sep ="/", ) %>%
   pivot_longer(names_to = "date", 
@@ -131,8 +129,149 @@ JH_conftime_clean <- JH_conftime_clean %>%
  
   JH_conftime_clean <- JH_conftime_clean %>%
   group_by(`Country/Region`, Lat, Long, date) %>% 
-  summarise(conf_COVID = sum(`Number of confirmed COVID-19`)) 
+  summarise(conf_COVID = sum(`Number of confirmed COVID-19`))  
 
+  
+    
+# COVID-19 deaths, in time series. 
+# Unifying latitude and longitude for transformation to state of capital, and tidying data by pivot_long  
+  JH_deadtime_clean <- JH_deadtime 
+  
+  JH_deadtime_clean <- JH_deadtime_clean %>% 
+    unite(Lat, Long,
+          col = Lat_Long, sep = "/") %>% 
+    mutate(Lat_Long = case_when(
+      str_detect(Lat_Long, "-35.4735/149.0124") ~ "-33.8688/151.2093", 
+      str_detect(Lat_Long, "-28.0167/153.4") ~ "-33.8688/151.2093",  
+      str_detect(Lat_Long, "-41.4545/145.9707") ~ "-33.8688/151.2093", 
+      str_detect(Lat_Long, "-37.8136/144.9631") ~ "-33.8688/151.2093",
+      str_detect(Lat_Long, "-31.9505/115.8605") ~ "-33.8688/151.2093", 
+      str_detect(Lat_Long, "-12.4634/130.8456") ~ "-33.8688/151.2093",
+      str_detect(Lat_Long, "-34.9285/138.6007")~ "-33.8688/151.2093",
+      str_detect(Lat_Long, "53.9333/-116.5765") ~ "49.2827/-123.1207",
+      str_detect(Lat_Long, "37.6489/-122.6655") ~ "49.2827/-123.1207",
+      str_detect(Lat_Long, "53.7609/-98.8139") ~ "49.2827/-123.1207",
+      str_detect(Lat_Long, "46.5653/-66.4619") ~ "49.2827/-123.1207",
+      str_detect(Lat_Long, "53.1355/-57.6604") ~ "49.2827/-123.1207",
+      str_detect(Lat_Long, "44.682/-63.7443") ~ "49.2827/-123.1207",
+      str_detect(Lat_Long, "51.2538/-85.3232") ~ "49.2827/-123.1207",
+      str_detect(Lat_Long, "46.5107/-63.4168") ~ "49.2827/-123.1207",
+      str_detect(Lat_Long, "52.9399/-73.5491") ~ "49.2827/-123.1207",
+      str_detect(Lat_Long, "52.9399/-106.4509") ~ "49.2827/-123.1207",
+      str_detect(Lat_Long, "31.8257/117.2264") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "30.0572/107.874") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "26.0789/117.9874") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "23.3417/113.4244") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "23.8298/108.7881") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "26.8154/106.8748") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "19.1959/109.7453") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "39.549/116.1306") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "47.862/127.7615") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "33.882/113.614") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "22.3/114.2") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "30.9756/112.2707") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "27.6104/111.7088") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "44.0935/113.9448") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "32.9711/119.455") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "27.614/115.7221") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "43.6661/126.1923") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "41.2956/122.6085") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "22.1667/113.55") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "37.2692/106.1655") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "35.7452/95.9956") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "35.1917/108.8701") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "36.3427/118.1498") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "31.202/121.4491") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "37.5777/112.2922") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "30.6171/102.7103") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "39.3054/117.323") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "31.6927/88.0924") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "41.1129/85.2401") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "24.974/101.487") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "29.1832/120.0934") ~ "40.1824/116.4142",
+      TRUE ~ Lat_Long)) %>% 
+    select(-`Province/State`) %>% 
+    separate(Lat_Long, into = c("Lat", "Long"), sep ="/", ) %>%
+    pivot_longer(names_to = "date", 
+                 values_to = "Number of COVID-19 related deaths",
+                 cols = -c("Country/Region", "Lat", "Long"),
+                 names_ptypes = (date = date())
+    )
+  
+  JH_deadtime_clean <- JH_deadtime_clean %>%
+    group_by(`Country/Region`, Lat, Long, date) %>% 
+    summarise(deaths_COVID = sum(`Number of COVID-19 related deaths`))
+  
+
+# COVID-19 recovery, in time series. 
+# Unifying latitude and longitude for transformation to state of capital, and tidying data by pivot_long    
+  
+  JH_recotime_clean <- JH_recotime 
+  
+  JH_recotime_clean <- JH_recotime_clean %>% 
+    unite(Lat, Long,
+          col = Lat_Long, sep = "/") %>% 
+    mutate(Lat_Long = case_when(
+      str_detect(Lat_Long, "-35.4735/149.0124") ~ "-33.8688/151.2093", 
+      str_detect(Lat_Long, "-28.0167/153.4") ~ "-33.8688/151.2093",  
+      str_detect(Lat_Long, "-41.4545/145.9707") ~ "-33.8688/151.2093", 
+      str_detect(Lat_Long, "-37.8136/144.9631") ~ "-33.8688/151.2093",
+      str_detect(Lat_Long, "-31.9505/115.8605") ~ "-33.8688/151.2093", 
+      str_detect(Lat_Long, "-12.4634/130.8456") ~ "-33.8688/151.2093",
+      str_detect(Lat_Long, "-34.9285/138.6007")~ "-33.8688/151.2093",
+      str_detect(Lat_Long, "53.9333/-116.5765") ~ "49.2827/-123.1207",
+      str_detect(Lat_Long, "37.6489/-122.6655") ~ "49.2827/-123.1207",
+      str_detect(Lat_Long, "53.7609/-98.8139") ~ "49.2827/-123.1207",
+      str_detect(Lat_Long, "46.5653/-66.4619") ~ "49.2827/-123.1207",
+      str_detect(Lat_Long, "53.1355/-57.6604") ~ "49.2827/-123.1207",
+      str_detect(Lat_Long, "44.682/-63.7443") ~ "49.2827/-123.1207",
+      str_detect(Lat_Long, "51.2538/-85.3232") ~ "49.2827/-123.1207",
+      str_detect(Lat_Long, "46.5107/-63.4168") ~ "49.2827/-123.1207",
+      str_detect(Lat_Long, "52.9399/-73.5491") ~ "49.2827/-123.1207",
+      str_detect(Lat_Long, "52.9399/-106.4509") ~ "49.2827/-123.1207",
+      str_detect(Lat_Long, "31.8257/117.2264") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "30.0572/107.874") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "26.0789/117.9874") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "23.3417/113.4244") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "23.8298/108.7881") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "26.8154/106.8748") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "19.1959/109.7453") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "39.549/116.1306") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "47.862/127.7615") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "33.882/113.614") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "22.3/114.2") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "30.9756/112.2707") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "27.6104/111.7088") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "44.0935/113.9448") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "32.9711/119.455") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "27.614/115.7221") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "43.6661/126.1923") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "41.2956/122.6085") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "22.1667/113.55") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "37.2692/106.1655") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "35.7452/95.9956") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "35.1917/108.8701") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "36.3427/118.1498") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "31.202/121.4491") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "37.5777/112.2922") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "30.6171/102.7103") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "39.3054/117.323") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "31.6927/88.0924") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "41.1129/85.2401") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "24.974/101.487") ~ "40.1824/116.4142",
+      str_detect(Lat_Long, "29.1832/120.0934") ~ "40.1824/116.4142",
+      TRUE ~ Lat_Long)) %>% 
+    select(-`Province/State`) %>% 
+    separate(Lat_Long, into = c("Lat", "Long"), sep ="/", ) %>%
+    pivot_longer(names_to = "date", 
+                 values_to = "Recovered from COVID-19 (no.)",
+                 cols = -c("Country/Region", "Lat", "Long"),
+                 names_ptypes = (date = date()))
+  
+  JH_recotime_clean <- JH_recotime_clean %>%
+    group_by(`Country/Region`, Lat, Long, date) %>% 
+    summarise(recov_COVID = sum(`Recovered from COVID-19 (no.)`)) 
+  
 ------------------------------------------------------------------------------------------------
   
 #UN datasets
@@ -255,6 +394,14 @@ mortality_pollution_related_clean <- read_tsv(file = "data/01_mortality_pollutio
 # ------------------------------------------------------------------------------
 #write_tsv(x = my_data_clean, path = "data/02_my_data_clean.tsv")
 
+write_tsv(x = COVID_test_clean, 
+          path = "data/02_COVID_test_clean.tsv" )
+write_tsv(x = JH_conftime_clean, 
+          path = "data/02_JH_conftime_clean.tsv" )
+write_tsv(x = JH_deadtime_clean, 
+          path = "data/02_JH_deadtime_clean.tsv" )
+write_tsv(x = JH_recotime_clean, 
+          path = "data/02_JH_recotime_clean.tsv" )
 write_tsv(x = adult_mortality_clean,
           path = "data/02_adult_mortality_clean.tsv")
 write_tsv(x = life_expectancy_clean,
--- R/03_augment.R
+++ R/03_augment.R
@@ -5,6 +5,7 @@ rm(list = ls())
 # Load libraries
 # ------------------------------------------------------------------------------
 library("tidyverse")
+library("readr")
 
 # Define functions
 # ------------------------------------------------------------------------------
@@ -12,7 +13,12 @@ source(file = "R/99_project_functions.R")
 
 # Load data
 # ------------------------------------------------------------------------------
-my_data_clean <- read_tsv(file = "data/02_my_data_clean.tsv")
+#my_data_clean <- read_tsv(file = "data/02_my_data_clean.tsv")
+
+COVID_test_clean <- read_tsv(file = "data/02_COVID_test_clean.tsv")
+JH_conftime_clean <- read_tsv(file = "data/02_JH_conftime_clean.tsv", col_types = cols(date = col_date(format="%m/%d/%y")))
+JH_deadtime_clean <- read_tsv(file = "data/02_JH_deadtime_clean.tsv", col_types = cols(date = col_date(format="%m/%d/%y")))
+JH_recotime_clean <- read_tsv(file = "data/02_JH_recotime_clean.tsv", col_types = cols(date = col_date(format="%m/%d/%y")))
 
 # Wrangle data
 # ------------------------------------------------------------------------------
