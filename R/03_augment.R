c1a1b92a73f35fb1c742400bd045d19e3c11b0e8
@s163636 <signemodvig@gmail.com>
2020-04-27 10:46:15 +0000

    antijoin

--- R/03_augment.R
+++ R/03_augment.R
@@ -20,10 +20,46 @@ POP_demo_clean <- read_tsv(file = "data/02_POP_demo_clean.tsv", col_types = cols
 JH_conftime_clean <- read_tsv(file = "data/02_JH_conftime_clean.tsv", col_types = cols(date = col_date(format="%m/%d/%y")))
 JH_deadtime_clean <- read_tsv(file = "data/02_JH_deadtime_clean.tsv", col_types = cols(date = col_date(format="%m/%d/%y")))
 JH_recotime_clean <- read_tsv(file = "data/02_JH_recotime_clean.tsv", col_types = cols(date = col_date(format="%m/%d/%y")))
+POP_demo_clean <- read_tsv(file = "data/02_POP_demo_clean.tsv" )
+smoking_clean <- read_tsv(file = "data/02_smoking_clean.tsv" )
+UN_pop_clean <- read_tsv(file = "data/02_UN_pop_clean.tsv" )
 
 # Wrangle data
 # ------------------------------------------------------------------------------
-my_data_clean_aug <- my_data_clean # %>% ...
+
+#Anti-join for test
+JH_conftime_clean <- JH_conftime_clean %>% 
+  rename('Country' = 'Country/Region')
+
+extra_countries_POP_demo <- POP_demo_clean %>% 
+  anti_join(JH_conftime_clean, by = 'Country') %>% 
+  count(Country, sort = T)
+
+extra_countries_JH <- JH_conftime_clean %>% 
+  anti_join(POP_demo_clean, by = 'Country') %>% 
+  count(Country, sort = T)
+
+#WHO datasets antijoin
+WHO_extra_countries <- POP_demo_clean %>% 
+  anti_join(smoking_clean, by = 'Country') %>% 
+  count(Country, sort = T)
+
+
+#WHO-UN datasets antijoin
+UN_pop_clean <- UN_pop_clean %>% 
+  rename('Country' = 'Country_Region')
+
+WHO_UN_extra_countries <- POP_demo_clean %>% 
+  anti_join(UN_pop_clean, by = 'Country') %>% 
+  count(Country, sort = T)
+
+#UN JH datasets antijoin
+UN_extra_countries <- UN_pop_clean %>% 
+  anti_join(JH_conftime_clean, by = 'Country') %>% 
+  count(Country, sort = T)
+
+rlang::last_error()
+
 
 # Write data
 # ------------------------------------------------------------------------------
