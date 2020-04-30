9b8958d226a880655d0541192a306f8febe8be12
MCHR001 <MCHR001>
2020-04-30 09:11:59 +0000

    small changes..to 02.clean

--- R/02_clean.R
+++ R/02_clean.R
@@ -60,7 +60,7 @@ BMI_above30 <- read_tsv(file = "data/01_BMI_above30_agestand_raw.tsv")
 # Wrangle data
 # ------------------------------------------------------------------------------
 ##Data in our world data
-#COVID-19 tests performed (cummulative data over time). Global data.
+#COVID-19 tests performed (cummulative data over time). Global data...
 
 COVID_test_clean <- COVID_test %>%
   separate(Entity, into = c("country", "waste"), sep ="-", ) %>% 
