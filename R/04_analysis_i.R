7713facd719a8e16280f62b00cd4436723952930
MCHR001 <MCHR001>
2020-05-03 10:26:36 +0000

    Added COVID-19 data vs. urbanization i 04_analysis

--- R/04_analysis_i.R
+++ R/04_analysis_i.R
@@ -5,10 +5,11 @@ rm(list = ls())
 # Load libraries
 # ------------------------------------------------------------------------------
 library("tidyverse")
+library("patchwork")
 
 # Define functions
 # ------------------------------------------------------------------------------
-source(file = "R/99_project_functions.R")
+# source(file = "R/99_project_functions.R")
 
 # Load data
 # ------------------------------------------------------------------------------
@@ -16,7 +17,14 @@ covid_aug <- read_tsv(file = "data/03_covid_aug.tsv")
 
 # Wrangle data
 # ------------------------------------------------------------------------------
-#my_data_clean_aug %>% ...
+##Tibble for Shiny app - see shiny app
+
+tibble_shiny <- covid_aug %>% 
+  group_by(country) %>% 
+  slice(which.max(date)) %>% 
+  select(country, dead_cases_per_100000, confirmed_cases_per_100000, `Population median age (years)`, density_of_hospitals, life_expectancy, `Population living in urban areas (%)`)
+
+
 
 # Model data
 # ------------------------------------------------------------------------------
@@ -24,28 +32,36 @@ covid_aug <- read_tsv(file = "data/03_covid_aug.tsv")
 
 # Visualise data
 # ------------------------------------------------------------------------------
-#my_data_clean_aug %>% ...
 
-#Visualising number of COVID-19 confirmed cases versus number of COVID-19 tests performed _MCHR001 
-#To be continued...
+#Overview of COVID-19 confirmed cases, COVID-19 realted deaths and COVID-19 tests performed for selected countries -  see shiny app
 
-conf_vs_test <- covid_aug %>%
-  mutate(conf_pop = `Number of confirmed COVID-19`/`Population (in thousands) total`) %>% 
-  mutate (dead_pop = `Number of COVID-19 related deaths`/`Population (in thousands) total`) %>% 
-  filter(country %in% c("US", "China", "Spain", "France", "Denmark", "Italy", "Sweden", "Norway", "Austria", "Belgium", "United Kingdom")) %>% 
-  group_by(date)
+# Is there a correlation between Covid-19 confirmed cases, deaths and recoveries and % people living in urban areas
 
-  ggplot(conf_vs_test, mapping = aes(x = date , y = conf_pop)) + 
-  geom_point(aes(color = country))
-  
-  
-  ggplot(conf_vs_test, mapping = aes(y = `Cumulative total per thousand`, x = date, na.rm = TRUE)) + 
-    geom_point(aes(color = country))
-  
-  ggplot(conf_vs_test, mapping = aes(y = dead_pop, x = date, na.rm = TRUE)) + 
-    geom_point(aes(color = country))
-  
+conf_cases_vs_urban <- covid_aug %>%
+  group_by(country) %>% 
+  slice(which.max(date)) %>% 
+  ggplot(conf_cases_vs_urban,mapping = aes(y = confirmed_cases_per_100000, x = `Population living in urban areas (%)`), na.rm = TRUE ) +
+  geom_point()
 
+conf_deaths_vs_urban <- covid_aug %>%
+  group_by(country) %>% 
+  slice(which.max(date)) %>% 
+  ggplot(conf_deaths_vs_urban, mapping = aes(y = dead_cases_per_100000 , x = `Population living in urban areas (%)`), na.rm = TRUE) +
+  geom_point()
+
+conf_recov_vs_urban <- covid_aug %>%
+  group_by(country) %>% 
+  slice(which.max(date)) %>% 
+  ggplot(conf_recov_vs_urban, mapping = aes(y = recov_cases_per_100000  , x = `Population living in urban areas (%)`), na.rm = TRUE) +
+  geom_point()
+
+# Utilizing the patchwork package for plot assembly
+
+conf_cases_vs_urban / conf_deaths_vs_urban / conf_recov_vs_urban + 
+  plot_annotation(
+    title = "COVID-19 confirmed cases, COVID-19 deaths, COVID-19 recovered (cummulative April 16th) vs. urbanisation in countries", 
+    subtitle = "No correlation, all variables increases in highly urbanised countries"
+  )
 
 # Write data
 # ------------------------------------------------------------------------------
