30cf06c94246c712e6f10540d163c9ffb1603263
hjhartling <hj@hartling.dk>
2020-05-06 23:06:05 +0000

    gif hj

--- R/04_analysis_iv.R
+++ R/04_analysis_iv.R
@@ -109,25 +109,85 @@ for(i in list_of_cov_hj){
 }
 
 
+#Making interactive plot
+#Population above 60 years old
+p_60years <- ggplot(covid_aug_by_country, aes(x = `population_aged_60_years_old_percentage`, y = `days_from_dec1_to_100_cases`,
+            color = log(gdp_per_capita_us_dollars), size=population_in_thousands_total, alpha=0.5, ids=country)) +
+  geom_point() +
+  scale_size(range = c(0.5, 20), name="Population") +
+  scale_colour_gradientn(colours=topo.colors(5), name = "GDP per capita") +
+  ylab("Number of confirmed Covid-19 cases") +
+  xlab("population aged above 60 years old (%)") +
+  ggtitle("Development of cases by country") +
+  guides(alpha="none")
 
-gg <- ggplot(covid_aug, aes(x = density_of_medical_doctors, y = `number_of_confirmed_covid-19`, color = sex, frame = date, size = population_in_thousands_total, ids=country)) +
-  geom_point()
-ggplotly(plot_test) %>% 
+#Population living in urban areas
+p_urban <- ggplot(covid_aug_by_country, aes(x = `population_living_in_urban_areas`, y = `days_from_dec1_to_100_cases`,
+                                              color = log(gdp_per_capita_us_dollars), size=population_in_thousands_total, alpha=0.5, ids=country)) +
+  geom_point() +
+  scale_size(range = c(0.5, 20), name="Population") +
+  scale_colour_gradientn(colours=topo.colors(5), name = "GDP per capita") +
+  ylab("Number of confirmed Covid-19 cases") +
+  xlab("Population living in urban areas (%)") +
+  ggtitle("Development of cases by country") +
+  guides(alpha="none")
+
+#Prevalence of smoking
+p_smoking <- ggplot(covid_aug_by_country, aes(x = `prevalence_smoking`, y = `days_from_dec1_to_100_cases`,
+                                            color = log(gdp_per_capita_us_dollars), size=population_in_thousands_total, alpha=0.5, ids=country)) +
+  geom_point() +
+  scale_size(range = c(0.5, 20), name="Population") +
+  scale_colour_gradientn(colours=topo.colors(5), name = "GDP per capita") +
+  ylab("Number of confirmed Covid-19 cases") +
+  xlab("Prevalence of smoking (%)") +
+  ggtitle("Development of cases by country") +
+  guides(alpha="none")
+
+ggplotly(p_60years, tooltip = c("country")) %>% 
   highlight("plotly_hover")
+ggplotly(p_urban, tooltip = c("country")) %>% 
+  highlight("plotly_hover")
+ggplotly(p_smoking, tooltip = c("country")) %>% 
+  highlight("plotly_hover")
+
+
+covid_aug2 <- covid_aug %>%
+  rename(covid_deaths = 'number_of_covid-19_related_deaths') %>%
+  rename(covid_cases = 'number_of_confirmed_covid-19') %>%
+  rename(date2 = date, Date)
 
  
 #Making gif showing progression of corona for each country
- plot_test <- ggplot(covid_aug, aes(y = `number_of_confirmed_covid-19`, x = `number_of_covid-19_related_deaths`)) +
-    geom_point(aes(color=sex, size=population_in_thousands_total, alpha=0.6)) + 
-    scale_size(range = c(.1, 16), name="Population") +
-    ylab("Confirmed Covid-19 cases") +
-    xlab("Density of medical doctors (unit)") +
-    ggtitle("Development of Corona-pandemic by country") +
-    guides(size="none", alpha="none") + 
+gif_plot <-ggplot(covid_aug2, aes_string(x="covid_cases", y = "covid_deaths")) +
+  geom_point(aes(color=log(gdp_per_capita_us_dollars), size=population_in_thousands_total, alpha=0.5)) + 
+  scale_size(range = c(0.5, 20), name="Population", labels = NULL) +
+  scale_colour_gradientn(colours=topo.colors(5), name = "GDP per capita") +
+  ylab("Covid deaths") +
+  xlab("covid_cases") +
+  ggtitle("Development of Corona-pandemic by country") +
+  guides(alpha="none") + 
   transition_time(date) +
     labs(title = "Date: {frame_time}")
 #Animate and present the files in  a gif
- animate(plot_test, duration = 10, fps = 10, width = 450, height = 450, renderer = gifski_renderer())
+ animate(gif_plot, duration = 20, fps = 10, width = 450, height = 450, renderer = gifski_renderer())
+ # save as a GIF
+ anim_save("results/04_analysis_iv/hj_plot_test.gif")
+ 
+ #Making gif showing progression of corona for each country - log-axis
+ gif_plot <-ggplot(covid_aug2, aes_string(x="covid_cases", y = "covid_deaths")) +
+   geom_point(aes(color=log(gdp_per_capita_us_dollars), size=population_in_thousands_total, alpha=0.5)) + 
+   scale_size(range = c(0.5, 20), name="Population", labels = NULL) +
+   scale_colour_gradientn(colours=topo.colors(5), name = "GDP per capita") +
+   scale_x_log10() +
+   scale_y_log10() +
+   ylab("Covid deaths") +
+   xlab("covid_cases") +
+   ggtitle("Development of Corona-pandemic by country") +
+   guides(alpha="none") + 
+   transition_time(date) +
+   labs(title = "Date: {frame_time}")
+ #Animate and present the files in  a gif
+ animate(gif_plot, duration = 10, fps = 10, width = 450, height = 450, renderer = gifski_renderer())
  # save as a GIF
  anim_save("results/04_analysis_iv/hj_plot_test.gif")
  
