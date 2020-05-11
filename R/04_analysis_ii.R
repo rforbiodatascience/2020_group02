aa51e33fbd687af835ee5bf5ca01112768992d28
metchr <mette.christoffersen.02@regionh.dk>
2020-05-11 07:53:38 +0000

    Rettelse til 04_analysis_ii

--- R/04_analysis_ii.R
+++ R/04_analysis_ii.R
@@ -6,6 +6,7 @@ rm(list = ls())
 # ------------------------------------------------------------------------------
 library("tidyverse")
 library("patchwork")
+library("PMCMRplus")
 
 
 # Define functions
